8bc0f105f0f013a355470fb9b2795a0e2979cf59
Rose Delcour-Min <rosedelcour-min@hotmail.co.uk>
2026-06-21 11:50:57 +0000

    Testing page title

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,9 +171,9 @@ def update_data(conn, df, changes):
 
 # Set the title that appears at the top of the page.
 """
-# :shopping_bags: Inventory tracker
+# :shopping_bags: Test title
 
-**Welcome to Alice's Corner Store's intentory tracker!**
+**Welcome to Rupert's Corner Store's intentory tracker!**
 This page reads and writes directly from/to our inventory database.
 """
 
